ad98b81ad50466852155f45863917c5bfa5846cd
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2022-09-24 21:24:58 +0200

    Add antd with examples (ant.design)

--- app/javascript/components/app/index.tsx
+++ app/javascript/components/app/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './styles.modules.scss'
 import './simple.scss'
 import './a-css.css'
+import { Button } from "antd"
+import { FastBackwardOutlined } from "@ant-design/icons"
 
 export function App() {
   return (
@@ -10,6 +12,8 @@ export function App() {
       <div className="internalDiv">
         some text here <span className="internalSpan">and in span</span>
       </div>
+      <Button type="primary">Button</Button>
+      <FastBackwardOutlined />
     </div>
   )
 }
